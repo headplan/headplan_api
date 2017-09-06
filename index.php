024abf5cd04a4290a7665abe04f313b8c1d270f8
headplan <headplan@163.com>
2017-09-06 11:27:51 +0800

    给参数默认值,并过滤数据

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-$input = $_GET['name'];
-printf('Hello %s', $input);
+$input = isset($_GET['name']) ? $_GET['name'] : 'World';
+header('Content-Type: text/html; charset=utf-8');
+printf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
