c9af315815e085b23deb7a2984b3d4028030db6c
headplan <headplan@163.com>
2017-09-08 03:57:39 +0800

    引入HttpKernel组件的控制器及参数解析器

--- public/index.php
+++ public/index.php
@@ -5,9 +5,10 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel;
 
 # 通用控制器,解耦模板渲染
-function render_template($request)
+function render_template(Request $request)
 {
     # 和请求有关的一些附加参数
     extract($request->attributes->all(), EXTR_SKIP);
@@ -21,18 +22,26 @@ $request = Request::createFromGlobals();
 
 # 引入路由配置
 $routes = include __DIR__.'/../routes/web.php';
-# 根据路由配置匹配URL路径
+# 根据路由配置匹配请求的URL路径
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+
+# 控制器解析器和控制器参数解析器
+$controllerResolver = new HttpKernel\Controller\ControllerResolver();
+$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
 # 模板判断改为异常判断
 try {
     # 添加附加请求参数,即pathinfo的数组形式.
     $request->attributes->add($matcher->match($request->getPathInfo()));
-    # 这里回调约定好的_controller匿名函数
-    # 从路由配置中获取
-    $_controller = $request->attributes->get('_controller');
-    $response = call_user_func($_controller ,$request);
+    # 这里是HttpKernel组件约定好的_controller回调或签名
+    # 控制器解析器根据请求获取签名对象和方法或回调
+    $_controller = $controllerResolver->getController($request);
+    # 控制器参数解析器
+    $_arguments = $argumentResolver->getArguments($request, $_controller);
+    # 回调控制器方法或匿名函数,他们都在路由文件中.
+    $response = call_user_func_array($_controller ,$_arguments);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,20 @@
 <?php
 use Symfony\Component\Routing;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 $routes = new Routing\RouteCollection();
 
 $routes->add('hello', new Routing\Route('/hello/{name}', [
     'name' => 'World',
-    '_controller' => function ($request) {
+    '_controller' => function (Request $request) {
         $response = render_template($request);
         $response->headers->set('Content-Type', 'text/plain');
         return $response;
     }
 ]));
 $routes->add('about', new Routing\Route('/about', [
-    '_controller' => function ($request) {
+    '_controller' => function (Request $request) {
         $request->attributes->set('test', '我是测试数据');
         $response = render_template($request);
         return $response;
@@ -31,19 +32,19 @@ function is_leap_year($year = null)
 
 class LeapYearController
 {
-    public function indexAction($request)
+    public function indexAction($year)
     {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('是闰年!'.'现在时间是:'.date('Y-m-d H:i:s', time()));
+        if (is_leap_year($year)) {
+            return new Response($year.'是闰年!'.'现在时间是:'.date('Y-m-d H:i:s', time()));
         }
 
-        return new Response('不是闰年!'.'现在时间是:'.date('Y-m-d H:i:s', time()));
+        return new Response($year.'不是闰年!'.'现在时间是:'.date('Y-m-d H:i:s', time()));
     }
 }
 
 $routes->add('leap-year', new Routing\Route('/leap-year/{year}', [
-    'year' => null,
-    '_controller' => array(new LeapYearController(), 'indexAction')
+    'year' => 2012,
+    '_controller' => 'LeapYearController::indexAction'
 ]));
 
 return $routes;
