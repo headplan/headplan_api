82bde7a426c551d7246999b687ab2df2b3d0ce42
headplan <headplan@163.com>
2017-09-14 22:37:04 +0800

    精简前端控制器

--- public/index.php
+++ public/index.php
@@ -3,45 +3,16 @@
 require_once __DIR__.'/../bootstrap/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Routing;
 use Symfony\Component\HttpKernel;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Headplan\Framework;
 
 $request = Request::createFromGlobals();
-# 实例化一个请求堆栈
-$requestStack = new RequestStack();
 
 # 引入路由配置
 $routes = include __DIR__.'/../routes/web.php';
 
-# 根据路由配置匹配请求的URL路径
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-# 控制器解析器和控制器参数解析器
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-# 创建dispatcher并注册一个监听到response事件
-$routes = include __DIR__.'/../config/listeners.php';
-
-# RouterListener实现和框架相同的逻辑,匹配进入的请求,再以路由参数来装载请求的属性
-$dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
-
-$listener = new HttpKernel\EventListener\ExceptionListener(
-    'Headplan\\Framework\\Controllers\\ErrorController::exceptionAction'
-);
-$dispatcher->addSubscriber($listener);
-# 确保响应兼容HTTP协议
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-# 支持流响应
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-# 返回字符串
-$dispatcher->addSubscriber(new Headplan\Events\StringResponseListener());
-
 # 实例化框架
-$framework = new Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+$framework = new Framework($routes);
 # HTTP缓存
 $framework = new HttpKernel\HttpCache\HttpCache(
     $framework,
--- src/Headplan/Framework.php
+++ src/Headplan/Framework.php
@@ -2,8 +2,49 @@
 
 namespace Headplan;
 
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\EventListener\ResponseListener;
+use Symfony\Component\HttpKernel\EventListener\StreamedResponseListener;
+use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
 use Symfony\Component\HttpKernel\HttpKernel;
 
 class Framework extends HttpKernel
 {
+    public function __construct($routes)
+    {
+        # 实例化一个请求堆栈
+        $requestStack = new RequestStack();
+
+        # 根据路由配置匹配请求的URL路径
+        $context = new RequestContext();
+        $matcher = new UrlMatcher($routes, $context);
+
+        # 控制器解析器和控制器参数解析器
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        # 订阅事件
+        $dispatcher = new EventDispatcher();
+        # RouterListener实现匹配进入的请求,再以路由参数来装载请求的属性
+        $dispatcher->addSubscriber(new RouterListener($matcher, $requestStack));
+        # 确保响应兼容HTTP协议
+        $dispatcher->addSubscriber(new ResponseListener('UTF-8'));
+        # 支持流响应
+        $dispatcher->addSubscriber(new StreamedResponseListener());
+        # 返回字符串
+        $dispatcher->addSubscriber(new Events\StringResponseListener());
+        # 自定义异常
+        $listener = new ExceptionListener(
+            'Headplan\\Framework\\Controllers\\ErrorController::exceptionAction'
+        );
+        $dispatcher->addSubscriber($listener);
+
+        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    }
 }
